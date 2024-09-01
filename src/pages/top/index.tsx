4e679203f9d471fab186b921b3f4012d272fba1b
nakamura0238 <fumiya.nakamura0108@gmail.com>
2024-09-01 15:03:13 +0900

    fix: トップページのレイアウトの修正

--- src/pages/top/index.tsx
+++ src/pages/top/index.tsx
@@ -6,19 +6,19 @@ export const Top = () => {
   return (
     <div className={styles.top_layout}>
       <div className={styles.top_container}>
-        <p>ポケモンカード用ツール</p>
-
-        <p>簡単にデッキレシピを作成できます</p>
-        <p>登録したデッキを比較して、共通のカードを確認できます</p>
+        <div className={styles.discription_container}>
+          <h1>ポケモンカード用ツール</h1>
+          <p>デッキレシピの作成と、デッキレシピの比較が行えるツールです。</p>
+        </div>
 
         <div className={styles.link_container}>
-          <Link to={"/create"}>デッキ登録</Link>
-          <Link to={"/deck"}>デッキ一覧</Link>
-          <Link to={"/compare"}>比較</Link>
+          <Link className={styles.primary} to={"/create"}>デッキ登録</Link>
+          <Link className={styles.primary} to={"/deck"}>デッキ一覧</Link>
+          <Link className={styles.primary} to={"/compare"}>比較</Link>
         </div>
 
         <div className={styles.notice_container}>
-          <p>注意</p>
+          <p className={styles.notice_label}>注意</p>
           <p>
             データはブラウザに保存されるため、デバイスごとに登録が必要になります
           </p>
